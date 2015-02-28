71231ecc1981c9adb7f8fc3046ad7ab339ffaef6
rezaabd <ra25544@email.vccs.edu>
2015-02-28 18:48:11 -0500

    add number

---
+++ Assignment2.java
@@ -0,0 +1,17 @@
+
+
+public class Assignment2 {
+	public static void main(String[] args){
+		
+		System.out.println(1/2);
+		System.out.println(1%2);
+		System.out.println(1.0/2);
+		System.out.println(5+7/2);
+		System.out.println("Beat" + 'a' + "Army");
+		System.out.println(6+13/5-35%3);
+		System.out.println(3.5*(5/4));
+		System.out.println((3.5*5)/4);
+		
+	}
+
+}
